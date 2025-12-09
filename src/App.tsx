ed4a41f8784a2dd186cf0577c78fa060e746e699
IbrahimOraby <orabyibrahim19@gmail.com>
2025-12-09 20:28:30 +0200

    feat: create the Hero section with video background and navbar updates
    - install gsap for animations
    - create Hero component with video background
    - update Navbar component with hover effects
    - modify index.css for custom colors
    - update package.json and package-lock.json with new dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 // import { useState } from "react";
 import "./App.css";
 import Layout from "./components/Layout/index";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Layout>Content</Layout>
+      <Layout>
+        <Hero />
+        <Highlights />
+      </Layout>
     </>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import srcVideo from "../assets/videos/iphone-16-pro-hero.mp4";
+
+function Hero() {
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      duration: 2,
+      delay: 0.5
+    });
+
+    gsap.to("#cta", {
+      opacity: 1,
+      duration: 1,
+      delay: 0.5
+    });
+
+  }, []);
+  return (
+    <section className="w-full  min-h-svh bg-custom-black relative pb-16">
+      <div className="w-full flex-center flex-col bg-custom-black relative">
+        <div className="absolute top-2/12 left-1/2 -translate-x-1/2">
+          <p
+            id="hero"
+            className="text-center font-semibold text-[28px] text-custom-white-100 opacity-0 "
+          >
+            iPhone 16 Pro
+          </p>
+        </div>
+        <div className="w-full h-[790px] overflow-hidden flex items-center justify-center">
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={srcVideo}
+            className="h-full w-full object-cover object-center"
+          >
+            <source src={srcVideo} type="video/mp4" />
+          </video>
+        </div>
+
+        <div id ="cta" className="flex flex-col items-center  absolute bottom-2/12 left-1/2 -translate-x-1/2 opacity-0">
+          <button className="bg-custom-blue text-custom-white-100 text-lg font-normal py-2 px-4 rounded-full cursor-pointer ">Buy</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
