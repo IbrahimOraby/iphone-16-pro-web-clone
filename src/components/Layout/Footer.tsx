750eea557553493e7b12e74a067e51f6d284d4a1
IbrahimOraby <orabyibrahim19@gmail.com>
2025-12-09 02:17:45 +0200

    feat: Update Layout Components and Styles
    - Updated Navbar component to adjust height and text styles for better visibility.
    - Modified Footer component to use custom gray color for background and text.

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-secondary text-white'>Footer</div>
+    <div className='bg-custom-gray-300 text-custom-gray-100'>Footer</div>
   )
 }
 
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <div className='bg-primary text-white'>Header</div>
-  )
+    <header className="bg-custom-black text-custom-gray-100 max-h-[44px]  flex items-center justify-center">
+      <Navbar />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
